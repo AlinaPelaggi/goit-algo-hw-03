fa28d88ca9ce5146c3413bd72e201c0fe3b81dcb
Alina Pelaggi <161222529+AlinaPelaggi@users.noreply.github.com>
2024-03-11 07:19:50 +0100

    Завдання 1 на перевірку

--- m3e1_updated.py
+++ m3e1_updated.py
@@ -8,11 +8,13 @@ def get_days_from_today(date: str) -> int:
         date_user = datetime.strptime(date_user, "%Y-%m-%d")
         days_user = datetime.toordinal(date_user) - datetime.toordinal(current_date)         #Рахуємо кількість днів між датами
         print(days_user)
-        return days_user       #Функція має повернути кількість днів між датами
+        
     except ValueError:
         print(f"ERROR  Date enterder wrong")
     except Exception: 
         pass
 
-date_user = "2024-12-12"
+    return days_user       #Функція має повернути кількість днів між датами
+
+date_user = "2024-12-20"
 get_days_from_today(date_user)
